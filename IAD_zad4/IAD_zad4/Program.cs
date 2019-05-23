6732d9d19f25139d39a38c4bed0cf21f3f7d36e6
MrLord9000 <filip1209348756@gmail.com>
2019-05-23 20:59:49 +0200

    Initial project structure added

---
+++ IAD_zad4/IAD_zad4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAD_zad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
